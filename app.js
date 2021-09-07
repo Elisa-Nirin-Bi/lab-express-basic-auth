f4d46bce2aa69841994010752454d1bb4884af06
Elisa Barontini <nirinproductions@gmail.com>
2021-09-07 16:21:40 +0100

    basic iteration done

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const createError = require('http-errors');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 const serveFavicon = require('serve-favicon');
-
+const expressSession = require('express-session');
+const MongoStore = require('connect-mongo');
+const mongoose = require('mongoose');
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -22,9 +24,26 @@ app.use(
   sassMiddleware({
     src: join(__dirname, 'public'),
     dest: join(__dirname, 'public'),
-    outputStyle: process.env.NODE_ENV === 'development' ? 'nested' : 'compressed',
+    outputStyle:
+      process.env.NODE_ENV === 'development' ? 'nested' : 'compressed',
     force: process.env.NODE_ENV === 'development',
     sourceMap: true
+  })
+);
+
+app.use(
+  expressSession({
+    // secret: process.env.SESSION_SECRET
+    secret: 'sdfgadsfasdf',
+
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 15 * 24 * 60 * 60 * 1000 // 15 days
+    },
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+      ttl: 60 * 60
+    })
   })
 );
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,80 @@
 const { Router } = require('express');
+const User = require('./../models/user');
+const bcryptjs = require('bcryptjs');
 const router = Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index');
+  console.log(req.session);
+  const userId = req.session.userId;
+  console.log(userId);
+  if (userId) {
+    User.findById(userId)
+      .then((user) => {
+        const message = `Hello ${user.username}`;
+        res.render('home', { title: message });
+      })
+      .catch((error) => {
+        next(error);
+      });
+  } else {
+    res.render('home', { title: 'Hello stranger' });
+  }
+});
+
+router.get('/register', (req, res, next) => {
+  res.render('register');
+});
+
+router.post('/register', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  bcryptjs
+    .hash(password, 10)
+    .then((passwordHashAndSalt) => {
+      return User.create({
+        username,
+        passwordHashAndSalt
+      });
+    })
+    .then((user) => {
+      console.log('New user created', user);
+      req.session.userId = user._id;
+      res.redirect('/register');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.get('/log-in', (req, res, next) => {
+  res.render('log-in');
+});
+
+router.post('/log-in', (req, res, next) => {
+  const { username, password } = req.body;
+  let user;
+  User.findOne({ username })
+    .then((document) => {
+      user = document;
+      if (!user) {
+        throw new Error('ACCOUNT_NOT_FOUND');
+      } else {
+        return bcryptjs.compare(password, user.passwordHashAndSalt);
+      }
+    })
+    .then((comparisonResult) => {
+      if (comparisonResult) {
+        console.log('User was authenticated');
+        req.session.userId = user._id;
+        res.redirect('/');
+      } else {
+        throw new Error('WRONG_PASSWORD');
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 module.exports = router;
